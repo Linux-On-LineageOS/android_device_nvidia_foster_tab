17794c96029a4d0c25ff63cb5e574d88bc192ed9
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:13:59 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -9,3 +9,4 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+$(call inherit-product, vendor/lindroid/lindroid.mk)
